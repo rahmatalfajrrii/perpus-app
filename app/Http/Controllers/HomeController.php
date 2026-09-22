c2e6679e6724d47c6ed10a008977dfe97517c0ec
Rahmat Al Fajri <rahmatalfajrrii@gmail.com>
2026-09-22 23:55:16 +0700

    pertemua 3 - Routing, Controller, dan Blade dasar

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $judul = 'Sistem infomasi Peminjaman Buku Perpustakaan Mini';
+        return view('home', ['judul' => $judul]);
+    }
+
+    public function tentang(){
+        return view('tentang');
+    }
+}
---
+++ resources/views/tentang.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title','Tentang Aplikasi')
+
+@section('content')
+    <h1>Tentang Aplikasi</h1>
+    <p>Aplikasi ini dibuat untuk memenuhi tugas mata kuliah pemrograman web II, berupa Sistem Informasi Peminjaman Buku Perpustakaan Mini</p>
+
+@endsection
